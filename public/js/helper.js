31a409f26b5d57c2bb85d68f9bcbaa7c55f5930d
Ashutosh Bhushan Srivastava <ashutosh.srivastava@nagarro.com>
2023-12-21 16:39:53 +0530

    adding date picker task

--- public/js/helper.js
+++ public/js/helper.js
@@ -334,7 +334,7 @@ function getinnerHtmlStructure(userConfigValues, nameValue= ''){
             innerHtml+= getinnerHtmlStructure(userConfigValues[i].dynamicAttributes,i+nameValue);
             //console.log("getinnerHtmlStructure: if condition output", innerHtml, userConfigValues[i].logicalOp);
         }else{
-            console.log("getinnerHtmlStructure: else condition ", i);
+            console.log("getinnerHtmlStructure: else condition ", i, userConfigValues[i]);
 
             if(i==0){
                 //console.log("getinnerHtmlStructure: else if condition ");
